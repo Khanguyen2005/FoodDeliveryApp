d86147ef6c708b57dcffaa55577bb94ce72764a9
Trần Duy Khoa <khoa992005@gmail.com>
2025-03-15 14:10:02 +0700

    Upgrade payment interface conversion

--- app/src/main/java/com/ltmb/ltmobile/services/OrderSuccessActivity.java
+++ app/src/main/java/com/ltmb/ltmobile/services/OrderSuccessActivity.java
@@ -11,13 +11,22 @@ import com.ltmb.ltmobile.R;
 
 public class OrderSuccessActivity extends AppCompatActivity {
     private Button btnBackToHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_success);
 
+        // Ánh xạ nút từ layout
+        btnBackToHome = findViewById(R.id.btnBackToHome);
 
-
-
+        // Xử lý sự kiện click nút "Quay về Trang Chủ"
+        btnBackToHome.setOnClickListener(v -> {
+            Intent intent = new Intent(OrderSuccessActivity.this, MainActivity.class);
+            intent.putExtra("OPEN_HOME", true); // Gửi thông tin mở HomeFragment
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP); // Tránh tạo nhiều instance của MainActivity
+            startActivity(intent);
+            finish(); // Đóng OrderSuccessActivity
+        });
     }
 }
